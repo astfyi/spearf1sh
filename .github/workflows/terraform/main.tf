fbdcab5532b52300e9b68054b243b36618e54d1a
Aleksandar Belchev <aleksandarbelchevdevop@gmail.com>
2022-07-13 13:55:00 +0300

    WIP

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -70,6 +70,7 @@ resource "aws_instance" "vm" {
       sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
       echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
       sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+      sudo usermod -aG docker ubuntu
 
       # Clone spearf1sh repository
       git clone --recurse-submodules https://github.com/advancedsecio/spearf1sh.git

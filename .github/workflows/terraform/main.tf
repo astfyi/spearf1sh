320056153378e99f0771365900080e5cdda3b006
Aleksandar Belchev <aleksandarbelchevdevop@gmail.com>
2022-07-13 14:02:28 +0300

    WIP

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -65,12 +65,14 @@ resource "aws_instance" "vm" {
 
   provisioner "remote-exec" {
     inline = [<<EOF
+      # Make ubuntu user sudoer
+      sudo usermod -aG sudo ubuntu
+
       # Install Docker
-      sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-release
-      sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-      sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-      sudo usermod -aG docker ubuntu
+      apt-get update && apt-get install -y ca-certificates curl gnupg lsb-release
+      mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+      apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
       # Clone spearf1sh repository
       git clone --recurse-submodules https://github.com/advancedsecio/spearf1sh.git

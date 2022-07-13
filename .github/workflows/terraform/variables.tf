6c962a78a656817554c861caa2f700d0f0afe9da
Aleksandar Belchev <aleksandarbelchevdevop@gmail.com>
2022-07-13 15:53:47 +0300

    WIP

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_access_key" {
   type        = string
   description = "AWS access key"
-  sensitive   = true
+#  sensitive   = true
 }
 
 variable "aws_secret_key" {
   type        = string
   description = "AWS secret key"
-  sensitive   = true
+#  sensitive   = true
 }
 
 variable "aws_region" {
   type        = string
   description = "AWS region"
-  sensitive   = true
+#  sensitive   = true
 }
 
 variable "gh_token" {
   type        = string
   description = "GitHub access token"
-  sensitive   = true
+#  sensitive   = true
 }

97059377d895b1251a5cd0431abc5e45ee5209c8
Josh Datko <jbd@cryptotronix.com>
2023-05-07 01:01:37 -0600

    adds litex mainly for litex term. Added but untested

---
+++ os/package/python-litex/python-litex.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# python-litex
+#
+################################################################################
+
+PYTHON_LITEX_VERSION = 2022.12
+PYTHON_LITEX_SITE_METHOD = git
+PYTHON_LITEX_SITE = git@github.com:enjoy-digital/litex.git
+PYTHON_LITEX_LICENSE = MIT
+PYTHON_LITEX_LICENSE_FILES = LICENSE
+PYTHON_LITEX_INSTALL_STAGING = NO
+PYTHON_LITEX_INSTALL_TARGET = YES
+PYTHON_LITEX_SETUP_TYPE = setuptools
+
+
+$(eval $(python-package))
+$(eval $(host-python-package))
